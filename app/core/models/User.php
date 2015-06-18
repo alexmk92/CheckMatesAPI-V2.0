61a872404f27a4f38ff2985dc512f4607748929e
Alex Sims <alexander.sims92@gmail.com>
2015-06-18 16:19:34 +0100

    Added the User class

--- app/core/models/User.php
+++ app/core/models/User.php
@@ -2,8 +2,58 @@
 
 namespace Models;
 
+/*
+|--------------------------------------------------------------------------
+| User
+|--------------------------------------------------------------------------
+|
+| Singleton user object to persist the logged in users details
+|
+*/
+
 class User
 {
-    public function __construct()
-    {}
+
+    /*
+    |--------------------------------------------------------------------------
+    | Local vars
+    |--------------------------------------------------------------------------
+    |
+    | Declare any local variable which the class should access
+    | $instance   - the single instanciated instance of the class (singleton)
+    | $connection - the connection to the database
+    |
+    */
+
+    private static $instance;
+    private        $user;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Construct
+    |--------------------------------------------------------------------------
+    |
+    | Builds the database and sets the connection, applying the singleton
+    | pattern to allow one open instaciation socket to the DB
+    |
+    */
+
+    private function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fetch
+    |--------------------------------------------------------------------------
+    |
+    | Returns the logged in user
+    |
+    */
+
+    private function fetch()
+    {
+        return $this->user;
+    }
 }
